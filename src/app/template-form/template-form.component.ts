38e45867975ae44734dc579eaebd2425c0c7058f
afranio.silva <afranio.silva@castgroup.com.br>
2022-11-28 15:47:20 -0300

    Vinculo dados component e template usando ngModel

--- src/app/template-form/template-form.component.ts
+++ src/app/template-form/template-form.component.ts
@@ -7,13 +7,20 @@ import { Component, OnInit } from '@angular/core';
 })
 export class TemplateFormComponent implements OnInit {
 
+  usuario:any = {
+    nome:"afranio",
+    email:"afranio@email.com"
+  }
+
   constructor() { }
 
   ngOnInit(): void {
   }
 
   onSubmit(form:any){
-    console.log(form);    
+    console.log(form.value); //forma de submissão de dados 1 usando o form (template)
+    console.log(this.usuario); // forma de submissão 2 usando o objeto (component)
+        
   }
 
 }
